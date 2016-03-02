12d98bd51e768039a3fdaad7e77b4502c2831c28
lewisdgearing <lewisgearing@clear.net.nz>
2016-03-02 18:54:55 +1300

    Updated CheckLogin
    Removed Temp Pages and updated check login to output json

--- API/checklogin.php
+++ API/checklogin.php
@@ -1,12 +1,86 @@
 <?php
 session_start();
+include "dbconnect.inc.php";
+
+// Connect to mysqli
+$mysqli = new mysqli($host, $userMS, $passwordMS, $database);
+if ($mysqli->connect_errno) 
+{
+    echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+}
+
+$result = array();
+$errors = array();
+
 
 if(isset($_SESSION['userID']))
 {
 	$userID = $_SESSION['userID'];
-	echo "Hello user ID: " . $userID;
+	
+	//Pull user details from the db
+	if($stmt = $mysqli->prepare("SELECT userName, firstName, lastName, profileImage FROM profile WHERE userID = ?"))
+	{
+		// Bind parameters
+		$stmt->bind_param("i", $userID);
+		
+		// Execute Query
+		$stmt->execute();
+		
+		// Store result
+		$stmt->store_result();
+		
+		if($stmt->num_rows > 0)
+		{
+			// Bind parameters
+			$stmt->bind_result($userName, $firstName, $lastName, $profileImage);
+			
+			// Fill with values
+			$stmt->fetch();
+			
+			// Close stmt
+			$stmt->close();
+			
+			$userData = [
+			"firstName" => $firstName,
+			"lastName" => $lastName,
+			"userName" => $userName, 
+			"profileImage" => $profileImage,
+			];
+
+		}
+		else
+		{
+			$tempError = [
+			"code" => "C001",
+			"field" => "userID",
+			"message" => "User Profile not found",
+			];
+			array_push($errors, $tempError);
+		}
+	}
 }
 else
 {
-	echo "No your not go login!";
+	$tempError = [
+	"code" => "C002",
+	"field" => "userID",
+	"message" => "No User Logged in",
+	];
+	array_push($errors, $tempError);
 }
+
+if(count($errors) == 0) //If no errors user is logged in
+{	
+	$result["status"] = 200;
+	$result["message"] = "User Logged In";
+	$result["userData"] = $userData;
+}
+else
+{
+	$result["status"] = 400;
+	$result["message"] = "User not Logged in";
+	$result["errors"] = $errors;
+}
+
+echo json_encode($result);
+
